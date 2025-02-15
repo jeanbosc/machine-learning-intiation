e746bae589fc69b83e349956dfc36b80964dd577
Jean Bosc <jean.bosc.ozanam@gmail.com>
2025-02-15 13:12:08 +0100

    test

---
+++ main.py
@@ -0,0 +1,8 @@
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+
+print(np.array([1, 2, 3, 4, 5]))
+lreg = LinearRegression()
